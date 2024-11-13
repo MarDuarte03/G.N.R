30c08161cd1e4c7022cfd55c049c1ed7b98f4a1c
MarDuarte03 <marina.ailen.duarte@gmail.com>
2024-11-13 18:27:53 -0300

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,66 +45,27 @@ function letras(cancion) {
 }
 
 
-
-
-//cookies
-let cookiesAccepted = false;
-
-// Mostrar el banner de cookies si no se ha aceptado
-function showCookieBanner() {
-    const banner = document.getElementById('cookie-banner');
-    banner.style.display = 'block';
-}
-
-// Manejar la aceptación de cookies
-function acceptCookies() {
-    cookiesAccepted = true;
-    const banner = document.getElementById('cookie-banner');
-    banner.style.display = 'none';
-    enableScroll();
-}
-
-// Inhabilitar scroll
-function disableScroll() {
-    window.scrollTo(0, window.scrollY);
-}
-
-// Habilitar scroll
-function enableScroll() {
-    window.removeEventListener('scroll', checkScroll);
-}
-
-//posición de scroll
-function checkScroll() {
-    if (!cookiesAccepted && window.scrollY > window.innerHeight / 2) {
-        disableScroll();
-    }
-}
-
-//scroll
-window.addEventListener('scroll', checkScroll);
-
-//aceptar de cookies
-document.getElementById('accept-cookies').addEventListener('click', acceptCookies);
-
-// Mostrar si no se aceptaron cookies
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', () => {
+    const cookieBanner = document.getElementById('cookieBanner');
+    const acceptCookiesBtn = document.getElementById('acceptCookies');
+  
+    // Función para verificar si las cookies fueron aceptadas
+    const cookiesAccepted = localStorage.getItem('cookiesAccepted');
+  
     if (!cookiesAccepted) {
-        showCookieBanner();
+      // Si no han sido aceptadas, muestra el aviso y bloquea el scroll
+      document.body.classList.add('no-scroll');
+      cookieBanner.style.display = 'flex';
     }
-    setTimeout(function() {
-        document.getElementById("popup").style.display = "flex";
-    }, 2000)
-
-    document.getElementById("close-btn").onclick = function() {
-        document.getElementById("popup").style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == document.getElementById("popup")) {
-            document.getElementById("popup").style.display = "none";
-        }
-    }
-};
-
-
+  
+    // Evento para aceptar cookies
+    acceptCookiesBtn.addEventListener('click', () => {
+      // Guarda la preferencia del usuario en localStorage
+      localStorage.setItem('cookiesAccepted', 'true');
+  
+      // Oculta el aviso de cookies y permite el scroll
+      cookieBanner.style.display = 'none';
+      document.body.classList.remove('no-scroll');
+    });
+  });
+  
