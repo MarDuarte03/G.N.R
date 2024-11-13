596d681bb2d800733f3bd895ebf69a118423de9d
MarDuarte03 <marina.ailen.duarte@gmail.com>
2024-11-13 18:17:33 -0300

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,65 @@ function letras(cancion) {
 
 
 
+
+//cookies
+let cookiesAccepted = false;
+
+// Mostrar el banner de cookies si no se ha aceptado
+function showCookieBanner() {
+    const banner = document.getElementById('cookie-banner');
+    banner.style.display = 'block';
+}
+
+// Manejar la aceptación de cookies
+function acceptCookies() {
+    cookiesAccepted = true;
+    const banner = document.getElementById('cookie-banner');
+    banner.style.display = 'none';
+    enableScroll();
+}
+
+// Inhabilitar scroll
+function disableScroll() {
+    window.scrollTo(0, window.scrollY);
+}
+
+// Habilitar scroll
+function enableScroll() {
+    window.removeEventListener('scroll', checkScroll);
+}
+
+//posición de scroll
+function checkScroll() {
+    if (!cookiesAccepted && window.scrollY > window.innerHeight / 2) {
+        disableScroll();
+    }
+}
+
+//scroll
+window.addEventListener('scroll', checkScroll);
+
+//aceptar de cookies
+document.getElementById('accept-cookies').addEventListener('click', acceptCookies);
+
+// Mostrar si no se aceptaron cookies
+window.onload = function() {
+    if (!cookiesAccepted) {
+        showCookieBanner();
+    }
+    setTimeout(function() {
+        document.getElementById("popup").style.display = "flex";
+    }, 2000)
+
+    document.getElementById("close-btn").onclick = function() {
+        document.getElementById("popup").style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == document.getElementById("popup")) {
+            document.getElementById("popup").style.display = "none";
+        }
+    }
+};
+
+
